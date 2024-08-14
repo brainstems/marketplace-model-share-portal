cc4921e85ce3707b0464dc1f7d0edc504740f7c5
Facundo Mendez <facundo.mendez@live.com.ar>
2024-08-14 19:18:31 -0300

    fix timezone

--- src/app/(landing)/Cards/Cards.tsx
+++ src/app/(landing)/Cards/Cards.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -9,7 +9,7 @@ import "./_cards.css";
 import fontTitle from 'next/font/local'
 
 const title = fontTitle({ src: '../../../../public/assets/fonts/ClashDisplayBold.woff' })
- 
+
 interface CardProps {
   title: string;
   className: string;
@@ -17,30 +17,51 @@ interface CardProps {
   time: string;
 }
 
-const Card: React.FC<CardProps> = ({ title, className, date, time }) => (
-  <motion.div
-    initial={{ opacity: 0.6 }}
-    whileInView={{ opacity: 1 }}
-    viewport={{ once: true }}
-    transition={{
-      duration: 0.3,
-    }}
-    className={` overflow-hidden ${className}`}
-  >
-    <div className=" absolute top-[1.4rem] right-[1.6rem]">
-      <p className="font-bold text-[#fefefe] text-[1.1rem] backdrop-blur-[10px]">
-        {date}
-      </p>
-    </div>
-    <div className=" absolute bottom-[8.5rem] md:bottom-[6.5rem] w-[10rem] right-[-4rem] border border-[#797979] rounded-xl px-4 py-2 bg-[#00000049] backdrop-blur-[10px] border-r-0">
-      <p className="text-[.9rem] font-semibold">{time}</p>
-    </div>
+const Card: React.FC<CardProps> = ({ title, className, date, time }) => {
+  const [localTime, setLocalTime] = useState<string | null>(null);
 
-    <div className="text">
-      <h1>{title}</h1>
-    </div>
-  </motion.div>
-);
+  useEffect(() => {
+ 
+    const [hours, minutes, period] = time.split(/[: ]/);
+    let hoursIn24 = parseInt(hours, 10);
+    if (period === 'PM' && hoursIn24 < 12) hoursIn24 += 12;
+    if (period === 'AM' && hoursIn24 === 12) hoursIn24 = 0;
+
+
+    const artTime = new Date(`${date} ${hoursIn24}:${minutes} GMT-0300`);
+    
+ 
+    const localTimeString = artTime.toLocaleTimeString([], { 
+      hour: '2-digit', 
+      minute: '2-digit', 
+      hour12: true 
+    });
+
+    setLocalTime(localTimeString);
+  }, [date, time]);
+
+  return (
+    <motion.div
+      initial={{ opacity: 0.6 }}
+      whileInView={{ opacity: 1 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.3 }}
+      className={`overflow-hidden ${className}`}
+    >
+      <div className="absolute top-[1.4rem] right-[1.6rem]">
+        <p className="font-bold text-[#fefefe] text-[1.1rem] backdrop-blur-[10px]">
+          {date}
+        </p>
+      </div>
+      <div className="absolute bottom-[8.5rem] md:bottom-[6.5rem] w-[10rem] right-[-4rem] border border-[#797979] rounded-xl px-4 py-2 bg-[#00000049] backdrop-blur-[10px] border-r-0">
+        <p className="text-[.9rem] font-semibold uppercase">{localTime || time}</p>
+      </div>
+      <div className="text">
+        <h1>{title}</h1>
+      </div>
+    </motion.div>
+  );
+};
 
 const Cards: React.FC = () => {
   const settings = {
@@ -98,49 +119,7 @@ const Cards: React.FC = () => {
       className: "card card-3",
       date: "Sun, Aug 18",
       time: "12:30 PM",
-    } /* ,
-    {
-      title: "Valencia vs Barcelona",
-      className: "card card-4",
-      date: "Sat, Aug 17",
-      time: "4:30 PM",
-    } */ /* ,
-    {
-      title: "valladolid vs Espanyol",
-      className: "card card-5",
-      date: "Mon, Aug 19",
-      time: "2:00 PM",
-    },
-    {
-      title: "Real Betis vs Girona",
-      className: "card card-6",
-      date: "Thu, Aug 15",
-      time: "4:30 PM",
-    },
-    {
-      title: "Las Palmas vs Sevilla",
-      className: "card card-7",
-      date: "Fri, Aug 16",
-      time: "4:30 PM",
-    },
-    {
-      title: "Valencia vs Barcelona",
-      className: "card card-8",
-      date: "Sat, Aug 17",
-      time: "4:30 PM",
-    },
-    {
-      title: "Mallorca vs Real Madrid",
-      className: "card card-9",
-      date: "Sun, Aug 18",
-      time: "4:30 PM",
-    },
-    {
-      title: "Villarreal vs Atlético Madrid",
-      className: "card card-10",
-      date: "Mon, Aug 19",
-      time: "4:30 PM",
-    }, */,
+    }
   ];
 
   const itemVariants = {
@@ -159,9 +138,9 @@ const Cards: React.FC = () => {
         initial="hidden"
         whileInView="visible"
         viewport={{ once: true }}
-        className="w-full flex justify-center lg:justify-center items-center relative overflow-hidden px-[2rem] lg:px-[8rem] xl:px-[11rem]  mb-[3rem] "
+        className="w-full flex justify-center lg:justify-center items-center relative overflow-hidden px-[2rem] lg:px-[8rem] xl:px-[11rem] mb-[3rem]"
       >
-        <h3 className={`${title.className} textPredictive text-[27px] sm:text-[35px] md:text-[2.8rem] font-[700] md:tracking-[-.5px] leading-[120%] md:leading-[120%] w-full md:w-[80%] text-center `}>
+        <h3 className={`${title.className} textPredictive text-[27px] sm:text-[35px] md:text-[2.8rem] font-[700] md:tracking-[-.5px] leading-[120%] md:leading-[120%] w-full md:w-[80%] text-center`}>
           Predictive Event Selections
         </h3>
       </motion.div>
