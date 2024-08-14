9aa279e7b9bca518d7cceb2e8704f657e4773745
Facundo Mendez <81197356+FacundoMendez@users.noreply.github.com>
2024-08-14 10:15:59 -0300

    Update Footer.tsx

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className="flex justify-center items-center flex-col   px-[1rem] md:px-[4rem] lg:px-[8rem] py-[3rem] pb-[1rem] mb-4 mt-[4rem] ">
       
       <div className="w-full h-[.1rem] bg-[#ffffff28] "></div>
-      <span className="mt-[1rem] text-[#ffffffaf] text-[.9rem] text-center">© 2024 JedAI | All Rights reserved. </span>
+      <span className="mt-[1rem] text-[#ffffffaf] text-[.9rem] text-center">© 2024 Brainstems | All Rights reserved. </span>
     </div>
   );
 };
